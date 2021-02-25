4557dce93e5aedfcc4c9fc8598c5f458dae13146
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2021-02-25 11:12:14 +0700

    add Mongodb pool configuration

--- config/mongo.go
+++ config/mongo.go
@@ -5,6 +5,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang-clean-architecture/exception"
+	"strconv"
 	"time"
 )
 
@@ -12,7 +13,22 @@ func NewMongoDatabase(configuration Config) *mongo.Database {
 	ctx, cancel := NewMongoContext()
 	defer cancel()
 
-	client, err := mongo.NewClient(options.Client().ApplyURI(configuration.Get("MONGO_URI")))
+	mongoPoolMin, err := strconv.Atoi(configuration.Get("MONGO_POOL_MIN"))
+	exception.PanicIfNeeded(err)
+
+	mongoPoolMax, err := strconv.Atoi(configuration.Get("MONGO_POOL_MAX"))
+	exception.PanicIfNeeded(err)
+
+	mongoMaxIdleTime, err := strconv.Atoi(configuration.Get("MONGO_MAX_IDLE_TIME_SECOND"))
+	exception.PanicIfNeeded(err)
+
+	option := options.Client().
+		ApplyURI(configuration.Get("MONGO_URI")).
+		SetMinPoolSize(uint64(mongoPoolMin)).
+		SetMaxPoolSize(uint64(mongoPoolMax)).
+		SetMaxConnIdleTime(time.Duration(mongoMaxIdleTime) * time.Second)
+
+	client, err := mongo.NewClient(option)
 	exception.PanicIfNeeded(err)
 
 	err = client.Connect(ctx)
