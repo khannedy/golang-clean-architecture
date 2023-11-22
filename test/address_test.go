1df7644e8cc5ae2280e2692d293f5edcd09a5e84
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-11-22 22:10:39 +0700

    draft unit test for address

--- test/address_test.go
+++ test/address_test.go
@@ -1,1 +1,43 @@
 package test
+
+import "testing"
+
+func TestCreateAddress(t *testing.T) {
+
+}
+
+func TestCreateAddressFailed(t *testing.T) {
+
+}
+
+func TestListAddresses(t *testing.T) {
+
+}
+
+func TestListAddressesFailed(t *testing.T) {
+
+}
+
+func TestGetAddress(t *testing.T) {
+
+}
+
+func TestGetAddressFailed(t *testing.T) {
+
+}
+
+func TestUpdateAddress(t *testing.T) {
+
+}
+
+func TestUpdateAddressFailed(t *testing.T) {
+
+}
+
+func TestDeleteAddress(t *testing.T) {
+
+}
+
+func TestDeleteAddressFailed(t *testing.T) {
+
+}
