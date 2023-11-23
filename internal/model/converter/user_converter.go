e129612c14e005f52b889576f35837e186ad7a83
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-11-24 06:00:11 +0700

    add converter

---
+++ internal/model/converter/user_converter.go
@@ -0,0 +1,30 @@
+package converter
+
+import (
+	"golang-clean-architecture/internal/entity"
+	"golang-clean-architecture/internal/model"
+)
+
+func UserToResponse(user *entity.User) *model.UserResponse {
+	return &model.UserResponse{
+		ID:        user.ID,
+		Name:      user.Name,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+	}
+}
+
+func UserToTokenResponse(user *entity.User) *model.UserResponse {
+	return &model.UserResponse{
+		Token: user.Token,
+	}
+}
+
+func UserToEvent(user *entity.User) *model.UserEvent {
+	return &model.UserEvent{
+		ID:        user.ID,
+		Name:      user.Name,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+	}
+}
--- internal/model/user_event.go
+++ internal/model/user_event.go
@@ -3,7 +3,6 @@ package model
 type UserEvent struct {
 	ID        string `json:"id,omitempty"`
 	Name      string `json:"name,omitempty"`
-	Token     string `json:"token,omitempty"`
 	CreatedAt int64  `json:"created_at,omitempty"`
 	UpdatedAt int64  `json:"updated_at,omitempty"`
 }
