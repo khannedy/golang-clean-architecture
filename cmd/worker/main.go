b9ea54ca8c7677ae64c46422f0cad6a096990707
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-08-07 00:57:59 +0700

    Refactor consumer setup into separate functions
    Extracted the logic for setting up user, contact, and address consumers into individual functions. This improves code readability and modularity by encapsulating the consumer initialization logic.

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 	"golang-clean-architecture/internal/config"
 	"golang-clean-architecture/internal/delivery/messaging"
 	"os"
@@ -17,22 +19,9 @@ func main() {
 
 	ctx, cancel := context.WithCancel(context.Background())
 
-	logger.Info("setup user consumer")
-	userConsumer := config.NewKafkaConsumer(viperConfig, logger)
-	userHandler := messaging.NewUserConsumer(logger)
-	go messaging.ConsumeTopic(ctx, userConsumer, "users", logger, userHandler.Consume)
-
-	logger.Info("setup contact consumer")
-	contactConsumer := config.NewKafkaConsumer(viperConfig, logger)
-	contactHandler := messaging.NewContactConsumer(logger)
-	go messaging.ConsumeTopic(ctx, contactConsumer, "contacts", logger, contactHandler.Consume)
-
-	logger.Info("setup address consumer")
-	addressConsumer := config.NewKafkaConsumer(viperConfig, logger)
-	addressHandler := messaging.NewAddressConsumer(logger)
-	go messaging.ConsumeTopic(ctx, addressConsumer, "addresses", logger, addressHandler.Consume)
-
-	logger.Info("Worker is running")
+	go RunUserConsumer(logger, viperConfig, ctx)
+	go RunContactConsumer(logger, viperConfig, ctx)
+	go RunAddressConsumer(logger, viperConfig, ctx)
 
 	terminateSignals := make(chan os.Signal, 1)
 	signal.Notify(terminateSignals, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
@@ -48,4 +37,25 @@ func main() {
 	}
 
 	time.Sleep(5 * time.Second) // wait for all consumers to finish processing
+}
+
+func RunAddressConsumer(logger *logrus.Logger, viperConfig *viper.Viper, ctx context.Context) {
+	logger.Info("setup address consumer")
+	addressConsumer := config.NewKafkaConsumer(viperConfig, logger)
+	addressHandler := messaging.NewAddressConsumer(logger)
+	messaging.ConsumeTopic(ctx, addressConsumer, "addresses", logger, addressHandler.Consume)
+}
+
+func RunContactConsumer(logger *logrus.Logger, viperConfig *viper.Viper, ctx context.Context) {
+	logger.Info("setup contact consumer")
+	contactConsumer := config.NewKafkaConsumer(viperConfig, logger)
+	contactHandler := messaging.NewContactConsumer(logger)
+	messaging.ConsumeTopic(ctx, contactConsumer, "contacts", logger, contactHandler.Consume)
+}
+
+func RunUserConsumer(logger *logrus.Logger, viperConfig *viper.Viper, ctx context.Context) {
+	logger.Info("setup user consumer")
+	userConsumer := config.NewKafkaConsumer(viperConfig, logger)
+	userHandler := messaging.NewUserConsumer(logger)
+	messaging.ConsumeTopic(ctx, userConsumer, "users", logger, userHandler.Consume)
 }
